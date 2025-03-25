7c6ebf0355a676eba3400a8b3795a961784b02ef
Seungseung <aprili3147@naver.com>
2025-03-25 11:42:50 +0900

    MFC 코드

---
+++ MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include <mysql.h>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+// CMFCApplication1Dlg 대화 상자
+class CMFCApplication1Dlg : public CDialogEx
+{
+public:
+    CMFCApplication1Dlg(CWnd* pParent = nullptr);
+
+#ifdef AFX_DESIGN_TIME
+    enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+    virtual BOOL OnInitDialog();
+    afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+    afx_msg void OnPaint();
+    afx_msg HCURSOR OnQueryDragIcon();
+    DECLARE_MESSAGE_MAP()
+
+private:
+    MYSQL* ConnectToDatabase();
+    void DisplayImage();  // ✅ 이미지 표시 함수 추가
+    std::string selectedFilePath;  // 파일 경로를 저장할 멤버 변수
+    void LoadImageFromDB(int id); // ✅ DB에서 이미지 불러오는 함수 추가
+
+public:
+    CListCtrl m_listHBS;
+    afx_msg void OnBnClickedButtonSelect();
+    void FetchAndDisplayHBSData();
+    afx_msg void OnFileFileopen();
+    afx_msg void OnItemClickListHBS(NMHDR* pNMHDR, LRESULT* pResult); // ✅ 리스트 클릭 이벤트 추가
+
+private:
+    HICON m_hIcon;          // 아이콘 핸들
+    CStatic m_pictureCtrl;  // ✅ Picture Control 변수
+    Mat InpImg, OutImg, TempImg;
+    unsigned char** GrayImg;
+    unsigned char** U, ** V;
+
+public:
+    afx_msg void OnBnClickedButton1();
+    bool UploadImageToDB(const cv::Mat& img);
+};
---
+++ MFCApplication1/resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication1.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       131
+#define IDC_BUTTON_SELECT               1000
+#define IDC_BUTTON_LOAD                 1001
+#define IDC_BUTTON1                     1001
+#define IDC_LIST1                       1002
+#define IDC_LIST_HBS                    1002
+#define IDC_PICTURE_CONTROL             1005
+#define m_PictureControl                1006
+#define IDC_PICTURE                     1009
+#define ID_FILE_FILEOPEN                32772
+#define ID_FILE_FILESAVE                32773
+#define ID_FILE_FILECLOSE               32774
+#define ID_FILE_MRU                     32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
